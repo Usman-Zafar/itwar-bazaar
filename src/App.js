b8070fe7a7276692b4f0215a4d519a0548af7ff8
Mohammad Usman Zafar <usmanzafar622@gmail.com>
2023-08-07 03:06:39 +0500

    Footer Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 // import { Signin } from "./Component/SigninComponent";
 // import { SignUp } from "./Component/SignupComponent";
 import { CustomerHeader } from "./Component/CustomerComponent/HeaderComponent";
+import { CustomerFooter } from "./Component/CustomerComponent/FooterCoponent";
 function App() {
   return (
     <div className="App">
       {/* <SignUp />
       <Signin /> */}
       <CustomerHeader />
+      <CustomerFooter />
     </div>
   );
 }
--- src/Component/CustomerComponent/HeaderComponent.jsx
+++ src/Component/CustomerComponent/HeaderComponent.jsx
@@ -37,7 +37,7 @@ export const CustomerHeader = () => {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ bgcolor: "green" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
